1155994c0b9b03e279bd2017033926aaa17c459f
yuri <yuri@shine.com>
2025-06-04 09:59:14 +0900

    first commit

---
+++ src/test/java/PrimeFactorTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimeFactorTest {
+
+    @Test
+    void name() {
+        assertEquals(1,1);
+    }
+}
