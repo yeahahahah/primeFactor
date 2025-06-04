fdb9d5a7d7d0fec94ce3463772c1aafea55f2dab
yuri <yuri@shine.com>
2025-06-04 10:22:21 +0900

    [feature] 32p

--- src/test/java/PrimeFactorTest.java
+++ src/test/java/PrimeFactorTest.java
@@ -1,11 +1,30 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class PrimeFactorTest {
+    PrimeFactor primeFactor;
+
+    @BeforeEach
+    void setUp() {
+        primeFactor = new PrimeFactor();
+    }
 
     @Test
-    void name() {
-        assertEquals(1,1);
+    void testPrimefactorOf1() {
+
+        assertEquals(Arrays.asList(),primeFactor.of(1));
+
+    }
+
+    @Test
+    void testPrimeFactorOf2() {
+
+        assertEquals(Arrays.asList(2),primeFactor.of(2));
     }
 }
